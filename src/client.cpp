6cbd5912cbec3a3d2cf5ce54acb25620179f422a
Danny Wamuya <dwamuya@gmail.com>
2023-05-24 19:59:12 +0300

    bug: fix missing header file include for menu.h

--- src/client.cpp
+++ src/client.cpp
@@ -9,29 +9,33 @@
 #include <pthread.h>
 
 #include "client.h"
+#include "menu.h"
 
 #define BUFFLENGTH 128
 #define USERNAMELEN 8
 
 static bool s_ClientStatus;
 
-void init_client(const char* ip, const char* port){
+void init_client(const char *ip, const char *port)
+{
     int sockfd;
 
-    if((sockfd = connect_to_socket(ip, port)) == -1){
+    if ((sockfd = connect_to_socket(ip, port)) == -1)
+    {
         std::cout << "\x1b[31mclient : did not connect to server\x1b[0m" << std::endl;
-        leave_server(false);
+        leave_server();
         return;
     }
 
     s_ClientStatus = true;
 
-    std::cout << "\x1b[32mclient : ready to communicate with server\x1b[0m\n" << std::endl;
+    std::cout << "\x1b[32mclient : ready to communicate with server\x1b[0m\n"
+              << std::endl;
 
     handle_server(&sockfd);
 }
 
-int connect_to_socket(const char* ip, const char* port)
+int connect_to_socket(const char *ip, const char *port)
 {
     int sockfd;
     struct addrinfo ref, *res, *faddr;
@@ -42,31 +46,36 @@ int connect_to_socket(const char* ip, const char* port)
     ref.ai_family = AF_UNSPEC;
     ref.ai_socktype = SOCK_STREAM;
 
-    if((addrstatus = getaddrinfo(ip, port, &ref, &res)) != 0){
+    if ((addrstatus = getaddrinfo(ip, port, &ref, &res)) != 0)
+    {
         fprintf(stderr, "\x1b[31mgetaddrinfo : %s\x1b[0m\n", gai_strerror(addrstatus));
         return -1;
     }
 
-    for(faddr = res; faddr != NULL; faddr = faddr->ai_next){
+    for (faddr = res; faddr != NULL; faddr = faddr->ai_next)
+    {
 
-        if((sockfd = socket(faddr->ai_family, faddr->ai_socktype, faddr->ai_protocol)) == -1){
+        if ((sockfd = socket(faddr->ai_family, faddr->ai_socktype, faddr->ai_protocol)) == -1)
+        {
             perror("\x1b[31mclient socket \x1b[0m");
             continue;
         }
 
-        if(connect(sockfd, faddr->ai_addr, faddr->ai_addrlen) == -1){
+        if (connect(sockfd, faddr->ai_addr, faddr->ai_addrlen) == -1)
+        {
             close(sockfd);
             perror("\x1b[31mclient connect \x1b[0m");
             continue;
         }
-        
-        inet_ntop(faddr->ai_family, client::cast_in_addr((struct sockaddr*)faddr->ai_addr), ipstr, INET6_ADDRSTRLEN);
+
+        inet_ntop(faddr->ai_family, client::cast_in_addr((struct sockaddr *)faddr->ai_addr), ipstr, INET6_ADDRSTRLEN);
         printf("\x1b[32mconnected to %s\x1b[0m\n", ipstr);
 
         break;
     }
 
-    if(faddr == NULL){
+    if (faddr == NULL)
+    {
         return -1;
     }
 
@@ -75,25 +84,31 @@ int connect_to_socket(const char* ip, const char* port)
     return sockfd;
 }
 
-void handle_server(int* sockfd){
+void handle_server(int *sockfd)
+{
     int sentbytes;
     char buf[BUFFLENGTH] = {0};
     char leave[] = "!leave";
-        
+
     pthread_t th;
-    pthread_create(&th, NULL, &recv_data, (void*)sockfd);
-    
+    pthread_create(&th, NULL, &recv_data, (void *)sockfd);
+
     std::cout << "\x1b[33m";
-    while(s_ClientStatus){
+    while (s_ClientStatus)
+    {
         memset(&buf, 0, BUFFLENGTH);
         fgets(buf, BUFFLENGTH, stdin);
-        
-        if(strncmp(leave, buf, 6) == 0){
+
+        if (strncmp(leave, buf, 6) == 0)
+        {
             s_ClientStatus = false;
             close(*sockfd);
             pthread_cancel(th);
-        } else {
-            if((sentbytes = send(*sockfd, buf, BUFFLENGTH, 0)) == -1){
+        }
+        else
+        {
+            if ((sentbytes = send(*sockfd, buf, BUFFLENGTH, 0)) == -1)
+            {
                 perror("\x1b[31mclient send \x1b[0m");
             }
         }
@@ -102,34 +117,45 @@ void handle_server(int* sockfd){
     pthread_join(th, NULL);
 }
 
-void* recv_data(void* sockfd)
-{   
+void *recv_data(void *sockfd)
+{
     int recvdbytes;
     char recvbuf[BUFFLENGTH];
 
-    while(s_ClientStatus){
-        if((recvdbytes = recv((*(int*)sockfd), recvbuf, BUFFLENGTH-1, 0)) <= 0){
-            if(recvdbytes == 0){
-                printf("\n\x1b[31mclient : server %d disconnected\x1b[0m\n", (*(int*)sockfd));
-                std::cout << "\x1b[34m\npress any key to leave...\x1b[0m" << std::endl;;
+    while (s_ClientStatus)
+    {
+        if ((recvdbytes = recv((*(int *)sockfd), recvbuf, BUFFLENGTH - 1, 0)) <= 0)
+        {
+            if (recvdbytes == 0)
+            {
+                printf("\n\x1b[31mclient : server %d disconnected\x1b[0m\n", (*(int *)sockfd));
+                std::cout << "\x1b[34m\npress any key to leave...\x1b[0m" << std::endl;
+                ;
                 s_ClientStatus = false;
                 return NULL;
-            } else if (recvdbytes == -1){
+            }
+            else if (recvdbytes == -1)
+            {
                 perror("\x1b[31mclient recv \x1b[0m");
             }
-        } else {
+        }
+        else
+        {
             recvbuf[recvdbytes] = '\0';
-            printf("%s", recvbuf);  
+            printf("%s", recvbuf);
         }
     }
     return NULL;
 }
 
-void* client::cast_in_addr(struct sockaddr* addr)
+void *client::cast_in_addr(struct sockaddr *addr)
 {
-    if(addr->sa_family == AF_INET){
-        return &(((struct sockaddr_in*)addr)->sin_addr);
-    } else {
-        return &(((struct sockaddr_in6*)addr)->sin6_addr);
+    if (addr->sa_family == AF_INET)
+    {
+        return &(((struct sockaddr_in *)addr)->sin_addr);
+    }
+    else
+    {
+        return &(((struct sockaddr_in6 *)addr)->sin6_addr);
     }
 }
